0eaab8a00e237114c64e603cbd57f0b2426eb527
Юля <>
2014-12-22 21:10:10 +0300

    Паттерны_СИТ

--- lomako/src/lomako/OrderIce.java
+++ lomako/src/lomako/OrderIce.java
@@ -8,7 +8,7 @@ public class OrderIce {
     public static void main(String[] args) {
        IceCreamStore nyStore=new IceCreamStore();        
         Ice icecream = nyStore.orderIceCream("Belarus");
-        System.out.println("The name of the ice cream is "+icecream.getName());
+        System.out.println("name of the ice cream is "+icecream.getName());
         System.out.println("For sale in "+icecream.getCountry());
         System.out.println("Number of "+icecream.getNumber());
         System.out.println("------------------------------------------------ ");
